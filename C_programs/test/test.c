88d55dfd18c42a31a77a226759269e0ae35ffc74
tcuc <infotcuc@gmail.com>
2015-09-04 14:18:52 +0200

    added a test program to demo a while loop.

--- C_programs/test/test.c
+++ C_programs/test/test.c
@@ -1,5 +1,15 @@
-#include "stdio"
+#include "stdio.h"
+//a program to demonstrate how a while loop works. 
+int x=0;
 
 int main()
 	{
+		while(x<10)
+		{
+			x++;
+			printf("%d",x);
+			//x++;
+		}
+		
+		return 0;
 	}
