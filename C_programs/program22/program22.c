87c4b9745e8f229a13e6df6d778c1567c107ee53
tcuc <infotcuc@gmail.com>
2015-09-14 19:19:47 +0200

    Wrote most of program 22, still needs to find the sum value if all stared items are sold.

--- C_programs/program22/program22.c
+++ C_programs/program22/program22.c
@@ -2,6 +2,9 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+#define Amount 2
+
+
 //## Function prototypes: ##
 int askForX(int);
 
@@ -9,27 +12,42 @@ int askForX(int);
 //## Main: ##
 int main()
 {
-	int innPris[10];
-	int utPris[10];
-	int lager[10];
+	int innPris[Amount];
+	float utPris[Amount];
+	int lager[Amount];
 	int i;
 	
-	for(i=0;i<10;i++)
-	{
-		/*
-		printf("pris på vare nr %d:",i+1);
-		scanf("%d")
-		*/
+	for(i=0;i<Amount;i++)
+	{		
+		system("clear");
+		printf("\nFør inn innkjøpspris for vare");
 		innPris[i]= askForX(i);
-		
+		printf("\nFør inn antall på lager for vare ");
+		lager[i]=askForX(i);
+		if(innPris[i]>30)
+		{
+			utPris[i]=innPris[i]*1.5;
+		}
+		else
+		{
+			utPris[i]=innPris[i]*2;
+		}
 	}
+	
+	for(i=0;i<Amount;i++)
+	{
+		printf("\n\n### Vare Nr %d: ###",i+1);
+		printf("\ninnkjøpspris:%d \nUtsalgspris: %.2f \nAntall på lager: %d",innPris[i],lager[i],utPris[i]);
+	}
+	
+	
+	return 0;
 }
 
-int askForX(int x;)
+int askForX(int x)
 {
 	int userInput;
-	system("clear");
-	printf("tast inn verdi for nr%d:",x);
+	printf(" nr%d:",x+1);
 	scanf("%d",&userInput);
 	return userInput;
 }
