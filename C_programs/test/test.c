acffa1ecc963a2b9a4c21764c952238271d6af91
tcuc <infotcuc@gmail.com>
2015-09-21 09:45:19 +0200

    wrote a test program to try to understand pointers.

--- C_programs/test/test.c
+++ C_programs/test/test.c
@@ -1,15 +1,50 @@
+
+//written by Tcuc (Thor Christan Cusick).
 #include "stdio.h"
-//a program to demonstrate how a while loop works. 
-int x=0;
+#include "stdlib.h"
+
+
+void func(int*);
 
 int main()
-	{
-		while(x<10)
-		{
-			x++;
-			printf("%d",x);
-			//x++;
-		}
-		
-		return 0;
-	}
+{
+	int b=0;
+	int *a = &b;
+	int A[]={3,5,7,9};
+	
+	
+	/*
+	printf("%d\n",a);
+	printf("%d\n",*a);
+	printf("%d\n",&a);
+	printf("%d\n",&b);
+	*/
+	
+	
+	printf("%d\n",*A);
+	printf("%d\n",*(A+1));
+	printf("%d\n",*(A+2));
+	printf("%d\n",*(A+3));
+	printf("-------\n");
+	
+	
+	
+	
+	/*
+	*A=13;
+	printf("b:%d\n",b);
+	printf("*A:%d\n",*A);
+	func(&b);
+	printf("*A: %d\n",*A);
+	printf("b: %d\n",b);
+	//printf("%d\n",b);
+	*/
+	
+	return 0;
+}
+
+void func(int *x)
+{
+	*x=99;
+	//printf("%d\n",*x);
+}
