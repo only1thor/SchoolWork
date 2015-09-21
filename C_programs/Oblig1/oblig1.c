7b1430a535e911d451658e2039b270772fdd53d8
tcuc <infotcuc@gmail.com>
2015-09-21 09:44:36 +0200

    wrote Oblig1, the "print a star" assignment. it's not minimal, so it needs more work. but it does the job.

---
+++ C_programs/Oblig1/oblig1.c
@@ -0,0 +1,43 @@
+//written by Tcuc (Thor Christan Cusick).
+#include "stdio.h"
+//#include "stdlib.h"
+
+
+void printFunc(char);
+
+int main()
+{
+	//varables:
+	
+	
+	for(int i=1;i<10;i++)
+	{
+		for(int x=9;x>i;x--)
+		{
+			printf(" ");
+			
+		}
+		for(int y=0;y<(i*2)-1;y++)
+		{
+			printf("%d",i);
+		}
+		printf("\n",i);
+	}
+	for(int i=8;i>0;i--)
+	{
+		for(int x=9;x>i;x--)
+		{
+			printf(" ");
+			
+		}
+		for(int y=0;y<(i*2)-1;y++)
+		{
+			printf("%d",i);
+		}
+		printf("\n",i);
+	}
+	
+}
+
+
+void printFunc(char x);
