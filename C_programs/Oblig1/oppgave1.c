d89404f279c6cd9545aa13b13a2b3548b0f0d922
tcuc <infotcuc@gmail.com>
2015-09-21 22:25:48 +0200

    wrote a document to answer the assignment, and wrote some more code on oppgave1.

---
+++ C_programs/Oblig1/oppgave1.c
@@ -0,0 +1,29 @@
+//written by Tcuc (Thor Christan Cusick).
+#include "stdio.h"
+
+int main()
+{
+	int sum=0;
+	for(int teller=10;teller<=25;teller++)
+		sum+=teller;
+	
+	printf("%d",sum);
+	/*
+	// del B)
+	int tall=2;
+	
+	if(tall%2)
+		printf("tall er oddetall:%d",tall);
+	else
+		printf("tallet er liketall:%d",tall);
+	*/
+	
+	
+	/*
+	// del A) 
+	for(int teller=20;teller>=10;teller--)
+	{
+		printf("%d\n",teller);
+	}
+	*/
+}
