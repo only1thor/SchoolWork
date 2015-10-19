dfaaaa0c749dbebcec8d2e3e874c75f09f73d653
tcuc <infotcuc@gmail.com>
2015-10-19 21:25:01 +0000

    wrote a test program to demo for Cam.

--- test/test2.c
+++ test/test2.c
@@ -1,5 +1,14 @@
-#include "stdio"
+#include "stdio.h"
 
 int main()
 	{
+		int i=0;
+		for (i=0;i<7;i++)
+		{
+			system("clear");
+			system("sleep 1s");
+			printf("\nhello world\n");
+			wait(2);
+			system("sleep 1s");
+		}
 	}
