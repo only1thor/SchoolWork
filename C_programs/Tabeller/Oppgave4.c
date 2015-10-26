f7ab4b70f262cb00a1e40b76420aceef12b0509e
tcuc <infotcuc@gmail.com>
2015-10-26 08:51:05 +0000

    finished Tabeller oppgave4. had to 0-out the table flaskeType.

--- C_programs/Tabeller/Oppgave4.c
+++ C_programs/Tabeller/Oppgave4.c
@@ -2,15 +2,45 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+//function prototypes:
+void Utskrift(int *,int);
+
+
+
 int main()
 {
-	int flaskeType[5];
+	int flaskeType[5]={0};
+	int input=0;
 	do 
 	{
-	    int input;
 	    printf("\nTast inn flaskeType(1-5):");
-	    scanf("%d",input);
-	    flaskeType[input-1]+=1
-	} while(innPris);
-	
+	    scanf("%d",&input);
+	    flaskeType[input-1]+=1;
+	} while(input>0);
+	Utskrift(flaskeType,5);
+	return 0;
+}
+
+
+//functions:
+
+void Utskrift(int table[],int a) //table is the input table, a is the length to read from the table.
+{
+	system("clear");
+	int totalAmmount=0;
+	float sumTotal=0;
+	float tempSum=0;
+	int i=0;
+	for(i=0;i<a;i++)
+	{
+		int x=0;
+		if (i<2){x=1;}
+		else {x=2;}
+		totalAmmount+=table[i];
+		tempSum=table[i]*x;
+		sumTotal+=tempSum;
+		printf("Antall flasker av type %d :%d gir kr: %.2f\n",i+1,table[i],tempSum);
+	}
+	for(i=0;i<30;i++){printf("-");}
+	printf("\nTOTAL %d flasker gir kr:%.2f",totalAmmount,sumTotal);
 }
